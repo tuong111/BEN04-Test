c5ce8ae79952b05c36b25e50382db50f9e919464
Jest <tuongtf@gmail.com>
2022-02-27 04:21:47 +0700

    nop bai

--- index.js
+++ index.js
@@ -27,28 +27,28 @@ let books = [{
     bcost : 5000
 }
 ]
-const MinShop = () => {
-    let choose = readline.question("Choose question : (a,b,c,d)")
-    switch (choose) {
-        case 'a':
-            let a = readline.question("Nhap tham so a")
-            let b = readline.question("Nhap tham so b")
-            MinShop1_a(a,b)
-            break;
-        case 'b' : 
-            let a = readline.question("Nhap tu khoa")
-            MinShop1_b(a)
-            break
-        case 'c':
-            MinShop1_c()
-            break
-        case 'd':
-            console.log(MinShop1_d())
-            break;
-        default:
-            break;
-    }
-}
+// const MinShop = () => {
+//     let choose = readline.question("Choose question : (a,b,c,d)")
+//     switch (choose) {
+//         case 'a':
+//             let a = readline.question("Nhap tham so a")
+//             let b = readline.question("Nhap tham so b")
+//             MinShop1_a(a,b)
+//             break;
+//         case 'b' : 
+//             let a = readline.question("Nhap tu khoa")
+//             MinShop1_b(a)
+//             break
+//         case 'c':
+//             MinShop1_c()
+//             break
+//         case 'd':
+//             console.log(MinShop1_d())
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
 
 const MinShop1_a = (a,b) => {
@@ -95,3 +95,83 @@ const MinShop1_d = () => {
 }
 
 
+let orders = [
+    { 
+        oCode : '1', 
+        customername : 'Tom',
+        address : {
+            province : 'HCM',
+            district : 'Q3',
+            homeAddress : '123'
+        },
+        orderLists : ['1','2']
+    },
+    { 
+        oCode : '2', 
+        customername : 'Joe',
+        address : {
+            province : 'Binh Duong',
+            district : 'Di An',
+            homeAddress : '456'
+        },
+        orderLists : ['3','5','1']
+    },
+    { 
+        oCode : '3', 
+        customername : 'Lisa',
+        address : {
+            province : 'Ha Noi',
+            district : 'Cau Giay',
+            homeAddress : '789'
+        },
+        orderLists : ['3','2','4','1']
+    },
+]
+
+const MinShop2_a = (x) => {
+    let result = []
+    for (let i = 0; i < orders.length; i++){
+        if (orders[i].orderLists.length >= x) {
+            result.push(orders[i])
+        }
+    }
+    return result
+}
+
+const MinShop2_b = () => {
+    let result = 0
+    for (let i = 0; i < orders.length; i++){
+        result += orders[i].orderLists.length
+    }
+    console.log(result)
+    return result
+}
+
+const MinShop2_c = () => {
+    let totalCost = 0
+    for (let i = 0; i <orders.length; i++) {
+        totalCost =+ Tinhtungdonhang(orders[i])
+    }
+    console.log(totalCost)
+    return totalCost    
+}
+
+
+const Tinhtungdonhang = (donhang) => {
+    let booksID = donhang.orderLists
+    let doanhthu1don = 0
+    for (let i = 0; i < booksID.length; i++){
+        doanhthu1don =+ LaytienbangID(booksID[i])
+    }
+    return doanhthu1don
+}
+
+const LaytienbangID = (id) => {
+    let cost = 0
+    for (let i = 0; i <books.length; i++) {
+        if (books[i].bcode === id) {
+            cost = books[i].bcost
+        }
+    }
+    return cost
+}
